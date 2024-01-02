7551ae0d78b9d66401f724b0f1b629409e5de702
noorhonjol <s12011175@stu.najah.edu>
2024-01-02 03:55:11 +0200

    Update DataCreation.java

--- untitled/src/main/java/CreationAndMetaData/DataCreation.java
+++ untitled/src/main/java/CreationAndMetaData/DataCreation.java
@@ -1,5 +1,6 @@
 package CreationAndMetaData;
 import Database.MongoDBSingleton;
+import Events.UserDataEvent;
 import MessageQueue.MockQueue;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -16,7 +17,6 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class DataCreation implements IDataCreation {
     private final Logger logger = LoggerFactory.getLogger(DataCreation.class);
-    private final Queue<Document> dataCollectionQueue = new LinkedBlockingQueue<>();
     private final MockQueue messageQueue = MockQueue.getInstance();
     private final MongoDBSingleton dbSingleton = MongoDBSingleton.getInstance();
     private final IUserService userService;
@@ -29,15 +29,25 @@ public class DataCreation implements IDataCreation {
 
         try {
             String userName = userProfile.getUserName();
+
             String userType = String.valueOf(userProfile.getUserType());
+
+
             Document userData = collectUserData(userName, userType);
+
+
             String status = userData.getString("status");
+
             storeMetaData(userName, userType, status);
+
             Document dataCollectionEvent = createDataCollectionEvent(userName, userType, userData);
-            dataCollectionQueue.add(dataCollectionEvent);
-            processDataCollectionQueue();
+
+            messageQueue.produce(new UserDataEvent(userName,dataCollectionEvent));
+
         } catch (Exception e) {
+
             logger.error("error during data collection for user: " + userProfile.getUserName(), e);
+
         }
     }
 
@@ -47,6 +57,7 @@ public class DataCreation implements IDataCreation {
                 .append("userType", userType)
                 .append("userData", userData);
     }
+
     @Override
     public Document getMetaData(String userName) {
         MongoDatabase database = dbSingleton.getDatabase("MyBase");
@@ -85,6 +96,7 @@ public class DataCreation implements IDataCreation {
             return UserType.NEW_USER;
         }
     }
+
     private Document collectUserData(String userName, String userType) {
         String status = "Pennding";
         Document userData = new Document();
@@ -93,22 +105,17 @@ public class DataCreation implements IDataCreation {
                 .append("status", status);
         return userData;
     }
-    public void processDataCollectionQueue() {
-        while (!dataCollectionQueue.isEmpty()) {
-            try {
-                Document event = dataCollectionQueue.poll();
-                messageQueue.produce("dataCollectionQueue", event);
-            } catch (Exception e) {
-                logger.error("Error processing data collection queue", e);
-            }
-        }
-    }
+
     public void storeMetaData(String userName, String userType, String status) {
         try {
             Document metaData = new Document("userName", userName)
                     .append("userType", userType)
                     .append("status", status);
-            dbSingleton.insert("MyBase", "MyColection", metaData);
+
+            MongoCollection<Document> collection =dbSingleton.getCollection("MyBase", "MyColection");
+
+            dbSingleton.insertNewUserDataInMongo(collection, metaData);
+
         } catch (Exception e) {
             logger.error("Error storing metadata for user: " + userName, e);
         }
